ec647fa2055d0171130f9639ec2ae8ebbd794f02
KingAnointig <adetoyeanointing15@gmail.com>
2023-05-06 11:06:13 +0100

    feat : add chat room for users and bot

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KingDaemonX/go-chat-BS
 
 go 1.19
+
+require golang.org/x/net v0.9.0
--- main.go
+++ main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
 type server struct {
 	conn map[*websocket.Conn]bool
+	msg  []byte
 }
 
-func NewServer() server {
-	return *&server{
+func newServer() *server {
+	return &server{
 		conn: make(map[*websocket.Conn]bool),
+		msg:  make([]byte, 1042),
+	}
+}
+
+func (s *server) startServer(ws *websocket.Conn) {
+	fmt.Printf("Starting Chat Server...🌀\n%s just joined the chat room\n", ws.RemoteAddr())
+
+	s.conn[ws] = true
+
+	s.chatRoom(ws)
+}
+
+func (s *server) chatRoom(ws *websocket.Conn) {
+	for {
+		if n, err := ws.Read(s.msg); err != nil {
+			if err == io.EOF {
+				fmt.Println("[❗]Error : ", err.Error())
+				break
+			}
+			fmt.Println("[❗]Error : ", err.Error())
+		} else {
+			ws.Write([]byte("--> Hello from KingDaemonX Bot...How may I help you 🤖"))
+			fmt.Printf("--> Client Message: %s", s.msg[:n])
+		}
 	}
 }
 
 func main() {
-	fmt.Println("Hello world from KingDaemonX 👋 \n This is a basic chat application backend service built with golang")
+	fmt.Println("Hello world from KingDaemonX 👋 \nThis is a basic chat application backend service built with golang")
+
+	server := newServer()
+
+	http.Handle("/chat", websocket.Handler(server.startServer))
+
+	http.ListenAndServe(":3000", nil)
 }
