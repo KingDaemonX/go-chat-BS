b1e3de9a51d23a722b6b8b9a2decb09e89c993a6
KingAnointig <adetoyeanointing15@gmail.com>
2023-05-06 10:38:54 +0100

    create server structure and function to initialize server

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/net/websocket"
+)
+
+type server struct {
+	conn map[*websocket.Conn]bool
+}
+
+func NewServer() server {
+	return *&server{
+		conn: make(map[*websocket.Conn]bool),
+	}
+}
 
 func main() {
 	fmt.Println("Hello world from KingDaemonX 👋 \n This is a basic chat application backend service built with golang")
